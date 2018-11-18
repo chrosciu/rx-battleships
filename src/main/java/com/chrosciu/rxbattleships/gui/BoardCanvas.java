aee635d6dee8e961b487df11ee941477d37306ef
Marcin Chrost <marcin.chrost@jcommerce.pl>
2018-11-18 15:13:32 +0100

    Refactor Board Canvas

--- src/main/java/com/chrosciu/rxbattleships/gui/BoardCanvas.java
+++ src/main/java/com/chrosciu/rxbattleships/gui/BoardCanvas.java
@@ -1,5 +1,6 @@
 package com.chrosciu.rxbattleships.gui;
 
+import com.chrosciu.rxbattleships.model.ShotWithResult;
 import com.chrosciu.rxbattleships.service.BattleService;
 import com.chrosciu.rxbattleships.config.Constants;
 import com.chrosciu.rxbattleships.model.ShotResult;
@@ -25,58 +26,69 @@ public class BoardCanvas extends JComponent {
     private static final float FONT_SIZE = 60;
 
     private ShotResult[][] shotResults = new ShotResult[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+    private boolean started;
     private boolean finished;
 
     @PostConstruct
     private void init() {
         finished = false;
         battleService.getBattleReadyMono()
-                .subscribe(null, null, () -> addMouseListener(mouseAdapter));
+                .subscribe(null, null, this::onShipsReady);
         battleService.getShotResultFlux()
-                .subscribe(shotResult -> {
-                    shotResults[shotResult.x][shotResult.y] = shotResult.shotResult;
-                    repaint();
-                }, null, () ->  {
-                    finished = true;
-                    removeMouseListener(mouseAdapter);
-                    repaint();
-                });
+                .subscribe(this::onShotPerformed, null, this::onAllShipsSunk);
+    }
+
+    private void onShipsReady() {
+        started = true;
+        addMouseListener(mouseAdapter);
+        repaint();
+    }
+
+    private void onShotPerformed(ShotWithResult shotWithResult) {
+        shotResults[shotWithResult.x][shotWithResult.y] = shotWithResult.shotResult;
+        repaint();
+    }
+
+    private void onAllShipsSunk() {
+        finished = true;
+        removeMouseListener(mouseAdapter);
+        repaint();
     }
 
     @Override
-    public void paintComponent(Graphics g) {
-        Font currentFont = g.getFont();
+    public void paintComponent(Graphics graphics) {
+        Graphics2D graphics2D = (Graphics2D)graphics;
+        Font currentFont = graphics2D.getFont();
         Font newFont = currentFont.deriveFont(FONT_SIZE);
-        g.setFont(newFont);
+        graphics2D.setFont(newFont);
+        graphics2D.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
+        Color color = started ? (finished ? Color.GREEN : Color.BLACK) : Color.RED;
+        graphics.setColor(color);
+        drawLabels(graphics2D);
+        drawFields(graphics2D);
+    }
 
-        if (finished) {
-            g.setColor(Color.GREEN);
+    private void drawLabels(Graphics2D graphics2D) {
+        for (char c = 'A'; c < 'A' + Constants.BOARD_SIZE; ++c) {
+            graphics2D.drawString(Character.toString(c),
+                    Constants.CELL_SIZE + (c - 'A') * Constants.CELL_SIZE,
+                    Constants.CELL_SIZE);
         }
+        for (int i = 0; i < Constants.BOARD_SIZE; ++i) {
+            graphics2D.drawString(Integer.toString(i),
+                    0,
+                    2 * Constants.CELL_SIZE + i * Constants.CELL_SIZE);
+        }
+    }
 
-        if (g instanceof Graphics2D) {
-            Graphics2D g2 = (Graphics2D)g;
-            g2.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
-
-            for (char c = 'A'; c < 'A' + Constants.BOARD_SIZE; ++c) {
-                g2.drawString(Character.toString(c),
-                        Constants.CELL_SIZE + (c - 'A') * Constants.CELL_SIZE,
-                        Constants.CELL_SIZE);
+    private void drawFields(Graphics2D graphics2D) {
+        for (int x = 0; x < Constants.BOARD_SIZE; ++x) {
+            for (int y = 0; y < Constants.BOARD_SIZE; ++y) {
+                char c = getFieldChar(shotResults[x][y]);
+                graphics2D.drawString(Character.toString(c),
+                        Constants.CELL_SIZE + Constants.CELL_SIZE * x,
+                        2 * Constants.CELL_SIZE + Constants.CELL_SIZE * y);
             }
-            for (int i = 0; i < Constants.BOARD_SIZE; ++i) {
-                g2.drawString(Integer.toString(i),
-                        0,
-                        2 * Constants.CELL_SIZE + i * Constants.CELL_SIZE);
-            }
-
-            for (int x = 0; x < Constants.BOARD_SIZE; ++x) {
-                for (int y = 0; y < Constants.BOARD_SIZE; ++y) {
-                    char c = getFieldChar(shotResults[x][y]);
-                    g2.drawString(Character.toString(c),
-                            Constants.CELL_SIZE + Constants.CELL_SIZE * x,
-                            2 * Constants.CELL_SIZE + Constants.CELL_SIZE * y);
-                }
-            }
-
         }
     }
 
